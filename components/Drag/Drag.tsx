064249814c79ead16268a80fab325c9df5473a54
Jhon <59811681+okayda@users.noreply.github.com>
2023-10-20 05:27:39 -0700

    Add an information to the Drag component if there is no board exist

--- components/Drag/Drag.tsx
+++ components/Drag/Drag.tsx
@@ -1,5 +1,6 @@
 "use client";
 import React from "react";
+import Image from "next/image";
 import { ComponentProps } from "@/types";
 import { updatePosition } from "@/redux/features/kanban-slice";
 import {
@@ -10,6 +11,7 @@ import {
 import { DragDropContext, Draggable, Droppable } from "@hello-pangea/dnd";
 import { onDragEnd } from "./DragHandler";
 
+import plus from "../../public/assets/plus.svg";
 import style from "./Drag.module.scss";
 
 export default React.memo(({ data, dispatch }: ComponentProps) => {
@@ -62,74 +64,84 @@ export default React.memo(({ data, dispatch }: ComponentProps) => {
   };
 
   return (
-    <div className={style.drag}>
-      <DragDropContext
-        onDragEnd={(result) => onDragEnd(result, columnsObj, updateDrag)}
-      >
-        {Object.entries(columnsObj).map(
-          ([columnId, column]: [string, any], index) => {
-            const length = column.items.values.length;
+    <div className={boardsLength ? style.drag : style.dragEmpty}>
+      {boardsLength ? (
+        <DragDropContext
+          onDragEnd={(result) => onDragEnd(result, columnsObj, updateDrag)}
+        >
+          {Object.entries(columnsObj).map(
+            ([columnId, column]: [string, any], index) => {
+              const length = column.items.values.length;
 
-            return (
-              <div key={index}>
-                <div className={style.drag__title}>
-                  <span className={style["drag__title--" + (index + 1)]}>
-                    &nbsp;
-                  </span>
-                  <h3>
-                    {column.name}({length})
-                  </h3>
-                </div>
+              return (
+                <div key={index}>
+                  <div className={style.drag__title}>
+                    <span className={style["drag__title--" + (index + 1)]}>
+                      &nbsp;
+                    </span>
+                    <h3>
+                      {column.name}({length})
+                    </h3>
+                  </div>
 
-                <Droppable droppableId={columnId} key={columnId}>
-                  {(provided, _) => {
-                    return (
-                      <div
-                        className={style.drag__column}
-                        {...provided.droppableProps}
-                        ref={provided.innerRef}
-                      >
-                        {column.items.values.map((item: any, index: any) => {
-                          return (
-                            <div
-                              className="card-item"
-                              onClick={() => showModalTask(item.itemId)}
-                              key={item.itemId}
-                            >
-                              <Draggable
-                                draggableId={item.itemId}
-                                index={index}
+                  <Droppable droppableId={columnId} key={columnId}>
+                    {(provided, _) => {
+                      return (
+                        <div
+                          className={style.drag__column}
+                          {...provided.droppableProps}
+                          ref={provided.innerRef}
+                        >
+                          {column.items.values.map((item: any, index: any) => {
+                            return (
+                              <div
+                                className="card-item"
+                                onClick={() => showModalTask(item.itemId)}
+                                key={item.itemId}
                               >
-                                {(provided, _) => {
-                                  return (
-                                    <div
-                                      className={style.drag__item}
-                                      ref={provided.innerRef}
-                                      {...provided.draggableProps}
-                                      {...provided.dragHandleProps}
-                                    >
-                                      <h4>{item.itemTitle}</h4>
+                                <Draggable
+                                  draggableId={item.itemId}
+                                  index={index}
+                                >
+                                  {(provided, _) => {
+                                    return (
+                                      <div
+                                        className={style.drag__item}
+                                        ref={provided.innerRef}
+                                        {...provided.draggableProps}
+                                        {...provided.dragHandleProps}
+                                      >
+                                        <h4>{item.itemTitle}</h4>
 
-                                      <p>
-                                        0 of {item.subTasks.length} subtasks
-                                      </p>
-                                    </div>
-                                  );
-                                }}
-                              </Draggable>
-                            </div>
-                          );
-                        })}
-                        {provided.placeholder}
-                      </div>
-                    );
-                  }}
-                </Droppable>
-              </div>
-            );
-          }
-        )}
-      </DragDropContext>
+                                        <p>
+                                          0 of {item.subTasks.length} subtasks
+                                        </p>
+                                      </div>
+                                    );
+                                  }}
+                                </Draggable>
+                              </div>
+                            );
+                          })}
+                          {provided.placeholder}
+                        </div>
+                      );
+                    }}
+                  </Droppable>
+                </div>
+              );
+            }
+          )}
+        </DragDropContext>
+      ) : (
+        <div className={style.dragEmpty__content}>
+          <p>This board is empty. Create a new column to get started.</p>
+          <button>
+            Create New Board
+            <Image src={plus} alt="" height={20} width={20} />
+          </button>
+        </div>
+      )}
 
       {boardsLength > 0 && (
         <div className={style.drag__emptyColumn}>
